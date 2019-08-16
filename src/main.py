84f694ba4656a045c1e7f7bf0938110cf893fbff
SchwarzeFalke <ca.varaperez@gmail.com>
2019-08-16 17:25:27 -0500

    Starting with main and lexer

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
+    # Start reading snippet code file for compiling
+    content = ''
+    with open('test.arcd', 'r') as file:
+        content = file.read()
+    print(content)
     pass
